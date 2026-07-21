e2aac4cee8e3ba96994d1a1c233b40d561083f7a
nuraffandyy <nuraffandyy@gmail.com>
2026-07-21 14:51:01 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "warehouse-locator-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./css/style.css",
+    "./js/app.js",
+    "./js/search.js",
+    "./data.json",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+
+    );
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+        .then(response => response || fetch(event.request))
+
+    );
+
+});
